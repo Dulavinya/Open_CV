c17ede16b87d9ae60f64e558fba4a22fd23eb0aa
Dulavinya <dulavinyamanawadu@gmail.com>
2025-04-14 22:43:00 +0530

    Update chapter4.py

--- chapter4.py
+++ chapter4.py
@@ -8,6 +8,6 @@ img = np.zeros((512, 512,3),np.uint8) # create a black imag
 cv2.line(img,(0,0),(img.shape[1],img.shape[0]),(0,255,0),3)
 cv2.rectangle(img,(0,0),(250,350),(0,0,255),cv2.FILLED)
 cv2.circle(img,(400,50),30,(255,255,0),5)   # draw a circle
-
+cv2.putText(img," OPENCV  ",(300,200),cv2.FONT_HERSHEY_COMPLEX,1,(0,150,0),3) # put text on the image
 cv2.imshow("Image", img) # show the image
 cv2.waitKey(0) # wait for a key press
