d14f98f28fb6ae3df11140ea16ab26a337b0ea3a
Dulavinya <dulavinyamanawadu@gmail.com>
2025-04-14 22:18:33 +0530

    Update chapter4.py

--- chapter4.py
+++ chapter4.py
@@ -6,7 +6,7 @@ img = np.zeros((512, 512,3),np.uint8) # create a black imag
 #img[200:300,200:300] = 255,0,0 # BGR color (blue)
 
 cv2.line(img,(0,0),(img.shape[1],img.shape[0]),(0,255,0),3)
-cv2.rectangle(img,(0,0),(250,350),(0,0,255),2)
+cv2.rectangle(img,(0,0),(250,350),(0,0,255),cv2.FILLED)
 cv2.circle(img,(400,50),30,(255,255,0),5)   # draw a circle
 
 cv2.imshow("Image", img) # show the image
